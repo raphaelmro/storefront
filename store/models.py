7b44eab5d2257c15f750391685d7aa1bc9c938a1
Mosh Hamedani <programmingwithmosh@gmail.com>
2021-06-07 18:04:26 -0400

    Fix the error with related name.

--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True)
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
