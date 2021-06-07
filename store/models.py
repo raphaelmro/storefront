2f3b6f35433b9d87cef708696a0faa3ed2532021
Mosh Hamedani <programmingwithmosh@gmail.com>
2021-06-07 11:56:25 -0400

    Resolving circular dependencies

--- store/models.py
+++ store/models.py
@@ -8,6 +8,7 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', null=True)
 
 
 class Product(models.Model):
